3e2a69f558dca4ff46f48c36257b2eb91c4025cf
Mattchu Pichu <34795746+K2X4B@users.noreply.github.com>
2026-04-22 14:56:36 -0400

    Add files via upload

---
+++ Prime Range Gen_001.py
@@ -0,0 +1,30 @@
+import re
+import time
+
+def Main():
+    s = int(input("Start of Range: "))
+    e = int(input("End of Range: "))
+    n = s
+    list = []
+    while n <= e:
+    
+        if not re.match(r'^.?$|^(..+?)\1+$', '1'*n):
+            print(n)
+            list.append(n)
+        n += 1
+    print("**************************")
+    print(len(list), "Prime Numbers Found!")
+    print("**************************")
+
+    print("Prime Numbers in Range: ")
+    print(list)
+  
+    print("**************************")
+#        time.sleep(0.25)
+
+if __name__ == "__main__":
+    print("*************************")
+    print("Prime Number Calculator")
+    print("*************************")
+    Main()
+#42
